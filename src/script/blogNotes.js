e3215300ce723174dcbae1aa2ce5842fa289be26
a240239114 <240239114@qq.com>
2019-07-12 17:04:03 +0800

    blogNotes页面适配完成

--- src/script/blogNotes.js
+++ src/script/blogNotes.js
@@ -1,22 +1,24 @@
 define([
     'jquery'
-], function ($) {
+], function($) {
 
     //点击按钮ul想左边移动
-    $(".form-horizontal button:contains('下一步')").toArray().forEach(function(item, i){
-            $(item).on("click",function(){
-                // console.log(i);
-                var distance = -(i+1)*840;
-                console.log(distance);
-                if(distance<-3360){
-                    $(".steps").css("transform","translate(0)");
-                }else{
-                    $(".steps").css("transform","translate("+distance+"px)");
-                }
+    $(".form-horizontal button:contains('下一步')").toArray().forEach(function(item, i) {
+        $(item).on("click", function() {
+            var viewW = $(".left3").width();
+            console.log(viewW);
+            // console.log(i);
+            var distance = -(i + 1) * viewW;
+            console.log(distance);
+            if (distance < -3360) {
+                $(".steps").css("transform", "translate(0)");
+            } else {
+                $(".steps").css("transform", "translate(" + distance + "px)");
+            }
 
-                // $('#main_nei').css("transform", "translate(0px,"+Height+")");
-                return false;
-            })
+            // $('#main_nei').css("transform", "translate(0px,"+Height+")");
+            return false;
+        })
     });
 
     //770-1200的适配
